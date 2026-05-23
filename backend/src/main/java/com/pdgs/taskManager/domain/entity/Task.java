42893ac77b889291f4589cb6a6005769a2d7b91d
Lopepi <lopepi@outlook.fr>
2026-05-23 14:45:19 +0200

    Create task entity

---
+++ backend/src/main/java/com/pdgs/taskManager/domain/entity/Task.java
@@ -0,0 +1,129 @@
+package com.pdgs.taskManager.domain.entity;
+
+import jakarta.persistence.Entity;
+
+import java.time.LocalDate;
+import java.time.Instant;
+import java.util.UUID;
+import java.util.Objects;
+
+@Entity
+public class Task {
+
+    private UUID id;
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private TaskStatus status;
+    private TaskPriority priority;
+    private Instant created;
+    private Instant updated;
+
+    public Task() {  
+    }
+
+    public Task(UUID id, String title, String description, LocalDate dueDate, TaskStatus status, TaskPriority priority, Instant created, Instant updated) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.status = status;
+        this.priority = priority;
+        this.created = created;
+        this.updated = updated;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setTaskStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public TaskPriority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(TaskPriority priority) {
+        this.priority = priority;
+    }
+
+    public Instant getCreated() {
+        return created;
+    }
+
+    public void setCreated(Instant created) {
+        this.created = created;
+    }
+
+    public Instant getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Instant updated) {
+        this.updated = updated;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass())
+        {
+            return false;
+        }
+
+        Task task = (Task) o;
+        return Objects.equals(id, task.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" + 
+            "id=" + id +
+            ", title" + title + '\'' +
+            ", description" + description + '\'' +
+            ", dueDate=" + dueDate +
+            ", status=" + status +
+            ", priority=" + priority +
+            ", created=" + created +
+            ", updated=" + updated +
+            '}';
+    }
+}
