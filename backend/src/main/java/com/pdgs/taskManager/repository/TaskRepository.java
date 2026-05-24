59f196c8cfaa290331bd257d9ed4c8f41353dbf0
Lopepi <lopepi@outlook.fr>
2026-05-24 13:21:32 +0200

    Create task repository

---
+++ backend/src/main/java/com/pdgs/taskManager/repository/TaskRepository.java
@@ -0,0 +1,13 @@
+package com.pdgs.taskManager.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pdgs.taskManager.domain.entity.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task,UUID> {
+
+}
